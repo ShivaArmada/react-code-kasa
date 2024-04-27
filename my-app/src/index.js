51968ef0bba01f835cb5b28fb43bac8f91f68915
ShivaArmada <planckaertg@gmail.com>
2024-04-27 02:43:50 +0200

    bibliotheque react router

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "../src/routes/root"; // A voir si c'est vraiment utile
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
